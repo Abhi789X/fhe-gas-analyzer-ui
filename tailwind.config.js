f8e7e4049d36107582044428bbcaceaffdc52473
Abhi789X <87174484+Abhi789X@users.noreply.github.com>
2025-07-11 00:03:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        darkglass: "rgba(255,255,255,0.08)",
-      },
-      backdropBlur: {
-        xs: "2px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
